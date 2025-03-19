53672b7e4cc4c416a13604e5e8fea6123dc00439
Pravin <pravinavhad3597@gmail.com>
2025-03-19 22:49:54 +0530

    File Based Routing of About and Contact Page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,7 @@
+export default function ContactPage(){
+    return(
+        <div className="bg-black w-full h-screen text-white flex justify-center items-center">
+        <h1>Contact Page</h1>
+        </div>
+    )
+}
