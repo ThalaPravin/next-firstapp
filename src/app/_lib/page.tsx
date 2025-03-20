25800485139b46dfa9d868cc70848986d18d9c38
Pravin <pravinavhad3597@gmail.com>
2025-03-20 22:56:45 +0530

    Private Folder in Nextjs

---
+++ src/app/_lib/page.tsx
@@ -0,0 +1,7 @@
+export default function hiddenPage(){
+    return (
+        <div className="bg-black text-white w-full h-screen flex justify-center items-center">
+            <h1>Hidden </h1>
+        </div>
+    )
+}
