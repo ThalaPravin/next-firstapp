1caf1befc1aa350637207c0b4b6c7ccb7d34811a
Pravin <pravinavhad3597@gmail.com>
2025-03-20 23:29:46 +0530

    Route Groups

---
+++ src/app/(auth)/Login/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Login () {
+    return (
+        <div className = "bg-black text-white w-full h-screen flex justify-center items-center">
+            Login
+        </div>
+
+    )
+}
---
+++ src/app/(auth)/Register/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Register () {
+    return (
+        <div className = "bg-black text-white w-full h-screen flex justify-center items-center">
+            Register
+        </div>
+
+    )
+}
