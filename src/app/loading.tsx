17d8426ac4f6457b5deb556eb1fcb494f45986cc
Pravin <pravinavhad3597@gmail.com>
2025-03-21 11:06:48 +0530

    Loading Page Added

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function loadingPage(){
+    return (
+        <div className="font-extrabold flex justify-center items-center">
+            <h1> Loading ....</h1>
+        </div>
+    )
+}
